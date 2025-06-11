4e00cb59672bf689dfd6c5d1a3aaf5cac5e903fe
Jas0one <104396994@students.swinburne.edu.my>
2025-06-11 21:04:15 +0800

    Update registration.php

--- assign1/registration.php
+++ assign1/registration.php
@@ -12,16 +12,6 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// Create members table
-$sql = "CREATE TABLE IF NOT EXISTS members (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(25) NOT NULL,
-    last_name VARCHAR(25) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    login_id VARCHAR(10) NOT NULL UNIQUE,
-    password VARCHAR(25) NOT NULL,
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)";
 
 if (mysqli_query($conn, $sql)) {
   echo "Table 'members' created successfully or already exists";
